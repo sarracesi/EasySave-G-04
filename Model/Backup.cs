b81f48bf78222ae6d9e43c310f0ddf7352e15d84
sarracesi <Sarra.KHECHINE.dz@viacesi.fr>
2022-12-16 11:34:27 +0100

    Backup Model

---
+++ Model/Backup.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+namespace V_3._0.Model
+{
+    public class Backup : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+
+        }
+        private string backup_Name;
+
+        public string Backup_Name
+        {
+            get { return backup_Name; }
+            set { backup_Name = value;  OnPropertyChanged("Backup_Name"); }
+        }
+
+
+        private string folder_Name;
+
+        public string Folder_Name
+        {
+            get { return folder_Name; }
+            set { folder_Name = value; OnPropertyChanged("Folder_Name"); }
+        }
+
+
+        private string folder_Path;
+
+        public string Folder_Path
+        {
+            get { return folder_Path; }
+            set { folder_Path = value; OnPropertyChanged("Folder_Path"); }
+        }
+
+        private string destination_Path;
+
+        public string Destination_Path
+        {
+            get { return destination_Path; }
+            set { destination_Path = value; OnPropertyChanged("Destination_Path"); }
+        }
+
+
+
+
+
+    }
+}
---
+++ Model/BackupService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace V_3._0.Model
+{
+    class BackupService
+    {
+        private static List<Backup> ObjBackupsList;
+
+        public BackupService()
+        {
+            ObjBackupsList = new List<Backup>()
+            {
+                new Backup{Backup_Name= "save1",Folder_Name="Folder1", Folder_Path= "C:\\Users\\26\\Desktop\\Demo", Destination_Path= "C:\\Users\\26\\Desktop\\Demo\\Folder2"}
+            };
+        }
+
+        public List<Backup> GetAll()
+        {
+            return ObjBackupsList;
+        }
+
+        public bool Add(Backup ObjNewBackup)
+        {
+            ObjBackupsList.Add(ObjNewBackup);
+            return true;
+        }
+    }
+}
